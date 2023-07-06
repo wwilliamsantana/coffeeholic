73316505c0128ed457a0efeac37424a47e00addd
William <euwilliamsantana@gmail.com>
2023-07-06 12:07:30 -0300

    feat: Logo, name + Config Font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import logo from '../../public/logo.svg'
+import Image from 'next/image'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <main className="grid place-items-center m-4">
+      <header className="max-w-[1440px] my-7 mx-9 w-full">
+        <div className="flex items-center justify-center gap-2 bg-red-300">
+          <Image src={logo} alt="" className="w-14 h-14" />
+          <h1 className="font-caveatFont text-5xl text-white">Coffeeholic</h1>
+        </div>
+      </header>
+    </main>
+  )
 }
